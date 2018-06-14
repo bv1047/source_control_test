bdb0a6dbf76a7bcb7007c3bfcb6d32f6cb99319a
chadwatson77 <>
2018-06-14 15:19:52 -0400

    commit

--- Tables/dbo.locations.sql
+++ Tables/dbo.locations.sql
@@ -7,7 +7,8 @@ CREATE TABLE [dbo].[locations] (
 		[address1]     [varchar](100) COLLATE SQL_Latin1_General_CP850_BIN2 NULL,
 		[city]         [varchar](100) COLLATE SQL_Latin1_General_CP850_BIN2 NULL,
 		[state]        [varchar](2) COLLATE SQL_Latin1_General_CP850_BIN2 NULL,
-		[zip]          [varchar](15) COLLATE SQL_Latin1_General_CP850_BIN2 NULL
+		[zip]          [varchar](15) COLLATE SQL_Latin1_General_CP850_BIN2 NULL,
+		[country]      [varchar](50) COLLATE SQL_Latin1_General_CP850_BIN2 NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[locations] SET (LOCK_ESCALATION = TABLE)
